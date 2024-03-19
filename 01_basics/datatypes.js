75009607ca319767135098c4acc49ac716707664
Shahwar Ahmed Siddiqui <45732451+shahwar-11@users.noreply.github.com>
2024-03-19 14:05:20 +0000

    Datatypes

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,2 @@
+"use Strict";//Treat all JS Code as newer version
+//alert(3+3) //Error because we are using node js not browser
---
+++ 01_basics/variable.js
@@ -0,0 +1,10 @@
+const accountId=144553 //cannot change further
+let accountEmail= "test@gmail.com"
+var accountPassword="12355"
+accountCity="Jaipur"
+
+accountPassword =212121
+accountEmail="sas@gmail.com"
+accountCity="Ratlam"
+
+console.table([accountId,accountEmail,accountPassword,accountId])
